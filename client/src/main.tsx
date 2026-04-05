5b7deab67ab40038a95d550f4b39783e29e155e9
holistis <info@holistischadviseur.nl>
2026-04-05 21:10:27 +0200

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App";
 import "./index.css";
 import React from "react";
 
-const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = "pk_live_XXXXXXXXXXXX";
 
 const queryClient = new QueryClient({
   defaultOptions: {
