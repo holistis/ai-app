0e86003667d4a67515a0ba33d28fe46e5f7e7fce
holistis <info@holistischadviseur.nl>
2026-04-05 10:17:48 +0200

    Create SignInPage.tsx

---
+++ client/src/pages/SignInPage.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/clerk-react";
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50">
+      <SignIn routing="path" path="/sign-in" />
+    </div>
+  );
+}
